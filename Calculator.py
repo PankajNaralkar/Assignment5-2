5fb4a7bc33721387d3aef21b53ef63e1baf6dd54
PankajNaralkar <115575830+PankajNaralkar@users.noreply.github.com>
2022-11-13 23:21:27 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+class Calculator:
+    def __init__(self):
+        pass
+    def addition(self):
+        return(a+b)
+    def subtraction(self):
+        return(a-b)
+    def multiplication(self):
+        return(a*b)
+    def division(self):
+        return(a/b)
+a = 94
+b = 10
+obj = Calculator()
+print(obj.addition())
+print(obj.subtraction())
+print(obj.multiplication())
+print(obj.division())
